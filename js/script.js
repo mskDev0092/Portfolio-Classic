afabd96e09a52af7dbef038e3c33ad2cecb595d5
Shehzad Khan <102049709+mskDev0092@users.noreply.github.com>
2024-08-20 02:12:43 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// script.js
+function goToPage2() {
+  // Navigate to page static projects
+  window.location.href = 'pages/static.html';
+}
+// script.js
+function goToPage3() {
+  // Navigate to page static projects
+  window.location.href = 'pages/react.html';
+}
+// script.js
+function goToPage4() {
+  // Navigate to page static projects
+  window.location.href = 'pages/backend.html';
+}
